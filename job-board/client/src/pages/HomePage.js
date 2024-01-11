c452b68989c9c181ff81fa84bb7ced07e0f834d6
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-10 20:54:53 -0500

    Use getJobs query in HomePage

--- job-board/client/src/pages/HomePage.js
+++ job-board/client/src/pages/HomePage.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react';
 import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import { getJobs } from '../lib/graphql/queries';
 
 function HomePage() {
+  const [jobs, setJobs] = useState([])
+  useEffect(() => {
+    getJobs().then(data => {
+      setJobs(data)
+    })
+  }, [])
   return (
     <div>
       <h1 className="title">
