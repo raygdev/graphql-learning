fe855b7bc76da00fea096597e39c562e1b416e83
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-11 22:06:31 -0500

    Add GraphQLError to job field

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -11,7 +11,15 @@ export const resolvers = {
         },
         //first argument is root, not used here. Second argument is arguments object
         //passes the args from the defined type args.id is what we are looking for in this case
-        job: async (_root, args) => await getJob(args.id),
+        job: async (_root, args) => {
+            const job = await getJob(args.id)
+            if(!job) {
+                throw new GraphQLError("Could not find job with id " + args.id, {
+                    extensions: { code: "NOT_FOUND"}
+                })
+            }
+            return job
+        },
         company: async (_root, args) => {
             const company = await getCompany(args.id)
             if(!company) {
