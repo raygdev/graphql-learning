5d91ff9520cbffeeb4097afde965ecf27ca5d330
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-11 22:01:32 -0500

    Add custom GraphQL Error

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,3 +1,4 @@
+import { GraphQLError } from "graphql"
 import { getCompany } from "./db/companies.js"
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js"
 
@@ -11,13 +12,21 @@ export const resolvers = {
         //first argument is root, not used here. Second argument is arguments object
         //passes the args from the defined type args.id is what we are looking for in this case
         job: async (_root, args) => await getJob(args.id),
-        company: async (_root, args) => await getCompany(args.id)
+        company: async (_root, args) => {
+            const company = await getCompany(args.id)
+            if(!company) {
+              throw new GraphQLError('No Company found with id ' + args.id, {
+                extensions: { code: "NOT_FOUND"}
+              })
+            }
+            return company
+        }
     },
 
     Company: {
         jobs: (company) => getJobsByCompany(company.id)
     },
-    
+
     Job: {
         /**
          * resolver functions take multiple arguments
