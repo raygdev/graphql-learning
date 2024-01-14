f40f7a51b271ec9981e72e1cf5ff55342faaeaf0
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-14 16:46:59 -0500

    Create getContext and pass to server options

--- job-board/server/server.js
+++ job-board/server/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import { readFile } from "node:fs/promises"
 import { authMiddleware, handleLogin } from './auth.js';
 import { resolvers } from "./resolvers.js"
+import { getUser } from "./db/users.js";
 
 const PORT = 9000;
 
@@ -15,9 +16,23 @@ app.post('/login', handleLogin);
 
 const typeDefs = await readFile('./schema.graphql', 'utf8')
 
+async function getContext({ req }) {
+  if(req.auth) {
+    const user = await getUser(req.auth.sub)
+    return { user }
+  }
+  //getContext passed to context property for options to apolloMiddleware
+  //accepts req and res from express.
+  //the auth property comes from express-jwt package and passes the decoded
+  //value of the token down the middleware chain
+  // return { auth: req.auth }
+  return {}
+}
+
 const apolloServer = new ApolloServer({ typeDefs, resolvers })
 await apolloServer.start()
-app.use("/graphql", apolloMiddleware(apolloServer))
+//pass getContext to the options object
+app.use("/graphql", apolloMiddleware(apolloServer, { context: getContext}))
 
 
 app.listen({ port: PORT }, () => {
