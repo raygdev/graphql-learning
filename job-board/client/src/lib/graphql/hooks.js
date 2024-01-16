099f076688565d5e038da1ecd0a67cc73c02405b
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-15 21:41:11 -0500

    Import queries and create useJob hook

--- job-board/client/src/lib/graphql/hooks.js
+++ job-board/client/src/lib/graphql/hooks.js
@@ -1,9 +1,16 @@
 import { useQuery } from "@apollo/client"
-import { companyByIdQuery } from "./queries"
+import { companyByIdQuery, jobByIdQuery, getJobsQuery } from "./queries"
 
 export function useCompany(id) {
     const { data, loading, error } = useQuery(companyByIdQuery, {
       variables: { id }
     })
     return { company: data?.company, loading, error: Boolean(error)}
+  }
+
+  export function useJob(id) {
+    const { data, loading, error } = useQuery(jobByIdQuery, {
+        variables: { id }
+    })
+    return { job: data?.job, loading, error: Boolean(error)}
   }
