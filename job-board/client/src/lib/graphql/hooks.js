5fb9da2141de9daee7b46d76d174a897bdd03875
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-16 17:53:00 -0500

    Add offset and limit as params and variables

--- job-board/client/src/lib/graphql/hooks.js
+++ job-board/client/src/lib/graphql/hooks.js
@@ -15,8 +15,9 @@ export function useCompany(id) {
     return { job: data?.job, loading, error: Boolean(error)}
   }
 
-  export function useJobs() {
+  export function useJobs(limit, offset) {
     const { data, loading, error } = useQuery(getJobsQuery, {
+        variables: { limit, offset },
         fetchPolicy: "network-only"
     })
     return { jobs: data?.jobs, loading, error: Boolean(error)}
