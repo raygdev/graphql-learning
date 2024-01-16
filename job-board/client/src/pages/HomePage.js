09e432b71f841835964a841914c880480d882467
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-16 18:11:01 -0500

    Add state and calculate offset

--- job-board/client/src/pages/HomePage.js
+++ job-board/client/src/pages/HomePage.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import JobList from '../components/JobList';
 import { useJobs } from '../lib/graphql/hooks';
 
 const JOBS_PER_PAGE = 5
 
 function HomePage() {
-  const { jobs, loading, error } = useJobs(JOBS_PER_PAGE, 0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const { jobs, loading, error } = useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE)
   if(loading) {
     return <div>...Loading</div>
   }
