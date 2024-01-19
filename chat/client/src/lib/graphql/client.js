59bbb9ca8831742412677a9a37e4f16546d2136b
Raymond Greeley <raymond.greeley.dev@gmail.com>
2024-01-18 19:18:31 -0500

    Import functions for ws connections

--- chat/client/src/lib/graphql/client.js
+++ chat/client/src/lib/graphql/client.js
@@ -1,4 +1,8 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql'
+import { createClient as  createWsClient } from 'graphql-ws';
 import { getAccessToken } from '../auth';
 
 const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
